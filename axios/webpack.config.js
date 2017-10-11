9521adb6ee61c409f789098d32465aed08eed984
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-10-11 22:10:41 +0100

    Update webpack.config.js

--- axios/webpack.config.js
+++ axios/webpack.config.js
@@ -20,7 +20,7 @@ function generateConfig(name) {
 
   config.plugins = [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
     })
   ];
 
